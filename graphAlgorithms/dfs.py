47453f269c0d01fbc3dead938df1b61fccb1cc05
Calvin Low <calsaviour@gmail.com>
2016-10-18 19:27:14 +0800

    Add files and implemmentation of heap

--- graphAlgorithms/dfs.py
+++ graphAlgorithms/dfs.py
@@ -10,10 +10,10 @@ class BinaryTree(object):
         self.root = Node(root)
 
     def search(self, find_val):
-        return self.preorder_search(tree.root, find_val)
+        return self.preorder_search(self.root, find_val)
 
     def print_tree(self):
-        return self.preorder_print(tree.root, "")[:-1]
+        return self.preorder_print(self.root, "")[:-1]
 
     def preorder_search(self, start, find_val):
         if start:
@@ -51,7 +51,7 @@ class BST(object):
                 current.left = Node(new_value)
 
     def search(self, find_val):
-            return self.search_helper(self.root, find_val)
+        return self.search_helper(self.root, find_val)
 
     def search_helper(self, current, find_val):
         if current:
@@ -65,7 +65,7 @@ class BST(object):
         return False
 
 
-if __name__ == "__main__":
+def test():
     # Set up tree
     tree = BinaryTree(1)
     tree.root.left = Node(2)
---
+++ graphAlgorithms/main.py
@@ -0,0 +1,13 @@
+__author__ = 'calvinlowyanming'
+import heap as heap
+import dfs as dfs
+import up_heapify as up_heapify
+import minAbsolute as minAbsolute
+import findMode as findMode
+
+if __name__ == "__main__":
+    heap.test()
+    dfs.test()
+    up_heapify.test()
+    minAbsolute.test()
+    findMode.test()
